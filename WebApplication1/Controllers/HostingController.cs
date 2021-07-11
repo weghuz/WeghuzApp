e36bfa3832e059f50732e3ca03021b37490aeacf
Rasmus Wallberg <rasmus@nordic-viewpoint.com>
2021-07-11 19:54:06 +0200

    Fix

--- WebApplication1/Controllers/HostingController.cs
+++ WebApplication1/Controllers/HostingController.cs
@@ -22,8 +22,8 @@ namespace WebApplication1.Controllers
         {
             switch(id)
             {
-                case "space": return File("/Images/space.jpg", "image/jpeg");
-                case "ratcard": return File("/Images/ratcard.png", "image/jpeg");
+                case "space": return File("/Images/space.jpg", "image/png");
+                case "ratcard": return File("/Images/ratcard.png", "image/png");
                 case "catcard": return File("/Images/catcard.png", "image/jpeg");
             }
             return File("", "image/jpeg");
