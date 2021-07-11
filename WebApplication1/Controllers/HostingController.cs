ba7f2efcfeeae21f0211021467b719345f529b08
Rasmus Wallberg <rasmus@nordic-viewpoint.com>
2021-07-11 18:31:27 +0200

    Added space file hosting test.
    Added space file hosting test.

---
+++ WebApplication1/Controllers/HostingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Web;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/{controller}/Display/{id}")]
+        public FileResult Display(string id)
+        {
+            switch(id)
+            {
+                case "space": return File("/Images/space.jpg", "image/jpeg");
+            }
+            return File("", "image/jpeg");
+        }
+    }
+}
